bfc35b41fc7b310665fc79c35adf9226cdf34238
FecheteMara <mara.fechete@yahoo.ro>
2022-10-11 23:41:02 +0300

    Second iteration of my weather app

--- app.js
+++ app.js
@@ -4,36 +4,42 @@ const bodyParser = require("body-parser");
 require('dotenv').config();
 
 const app = express();
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname+"/index.html");
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
     const query = req.body.cityName;
     // Calling the API key
     const apiKey = process.env.API_KEY;
     const unit = "metric";
-    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit;
-    https.get(url, function(response){
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+    https.get(url, function (response) {
         console.log(response.statusCode);
-        response.on("data", function(data){
+        response.on("data", function (data) {
             const weatherData = JSON.parse(data);
             const description = weatherData.weather[0].description;
             const temp = weatherData.main.temp;
             const icon = weatherData.weather[0].icon;
-            const imgURL = 'http://openweathermap.org/img/wn/'+icon+'@2x.png';
-            res.write("<h1>The temperature in " + query + " is: " + temp + " degrees Celcius.</h1>");
-            res.write("<h3>The weather is currently "+ description + ".</h3>");
-            res.write("<img src=" + imgURL + ">");
-            res.send();
+            const imgURL = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+            res.render("response", {
+                city: query.toUpperCase(),
+                temperature: temp,
+                desc: description,
+                imgURL: imgURL
+            });
         });
     });
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server is running on port 3000.");
 });
